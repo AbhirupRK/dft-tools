a77f9801bb6a377a6bdf4c125d7d72085705db3c
Abhirup Roy Karmakar <abhiruprk@gmail.com>
2025-06-30 22:12:02 +0200

    Fixed SIESTA species label bug

--- modules/dft.py
+++ modules/dft.py
@@ -70,7 +70,31 @@ class ReadVaspOutput:
 
     def get(self, key, default=None):
         return self.data.get(key, default)
-    
+
+# Just for SIESTA: to fetch the element symbol from the atomic number
+# as the Specie Labels may vary due to custom pseudopotential name
+periodic_table = {
+    1:  "H",    2:  "He",   3:  "Li",   4:  "Be",   5:  "B",    6:  "C",
+    7:  "N",    8:  "O",    9:  "F",    10: "Ne",  11: "Na",  12: "Mg",
+    13: "Al",   14: "Si",   15: "P",    16: "S",   17: "Cl",  18: "Ar",
+    19: "K",    20: "Ca",   21: "Sc",   22: "Ti",  23: "V",   24: "Cr",
+    25: "Mn",   26: "Fe",   27: "Co",   28: "Ni",  29: "Cu",  30: "Zn",
+    31: "Ga",   32: "Ge",   33: "As",   34: "Se",  35: "Br",  36: "Kr",
+    37: "Rb",   38: "Sr",   39: "Y",    40: "Zr",  41: "Nb",  42: "Mo",
+    43: "Tc",   44: "Ru",   45: "Rh",   46: "Pd",  47: "Ag",  48: "Cd",
+    49: "In",   50: "Sn",   51: "Sb",   52: "Te",  53: "I",   54: "Xe",
+    55: "Cs",   56: "Ba",   57: "La",   58: "Ce",  59: "Pr",  60: "Nd",
+    61: "Pm",   62: "Sm",   63: "Eu",   64: "Gd",  65: "Tb",  66: "Dy",
+    67: "Ho",   68: "Er",   69: "Tm",   70: "Yb",  71: "Lu",  72: "Hf",
+    73: "Ta",   74: "W",    75: "Re",   76: "Os",  77: "Ir",  78: "Pt",
+    79: "Au",   80: "Hg",   81: "Tl",   82: "Pb",  83: "Bi",  84: "Po",
+    85: "At",   86: "Rn",   87: "Fr",   88: "Ra",  89: "Ac",  90: "Th",
+    91: "Pa",   92: "U",    93: "Np",   94: "Pu",  95: "Am",  96: "Cm",
+    97: "Bk",   98: "Cf",   99: "Es",  100: "Fm", 101: "Md", 102: "No",
+   103: "Lr",  104: "Rf",  105: "Db",  106: "Sg", 107: "Bh", 108: "Hs",
+   109: "Mt",  110: "Ds",  111: "Rg",  112: "Cn", 113: "Nh", 114: "Fl",
+   115: "Mc",  116: "Lv",  117: "Ts",  118: "Og"
+}
 
 class ReadSiestaOutput:
     def __init__(self, file_content):
@@ -101,8 +125,7 @@ class ReadSiestaOutput:
                     pos_cart.append(linesplit[1:4]) ; ion_types.append(linesplit[4])
                 ion_types = np.array(ion_types) ; pos_cart = np.array(pos_cart) # New
                 sorted_indices = np.argsort(ion_types) # New
-                ion_types = ion_types[sorted_indices] # New
-                pos_cart = pos_cart[sorted_indices] # New
+                ion_types = ion_types[sorted_indices] ; pos_cart = pos_cart[sorted_indices] # New
                 elements, nions = np.unique(ion_types, return_counts=True)
                 self.data["nions"] = np.array(nions) ; natoms = sum(np.array(nions))
                 self.data["pos_cart"] = np.array(pos_cart).astype("float")*bohr2ang
@@ -110,7 +133,8 @@ class ReadSiestaOutput:
                 species = []
                 j = i+1
                 while "%endblock ChemicalSpeciesLabel" not in lines[j]:
-                    linesplit = lines[j].split() ; species.append(linesplit[2])
+                    linesplit = lines[j].split() #; species.append(linesplit[2])
+                    symbol = periodic_table[int(linesplit[1])] ; species.append(symbol) # New
                     j += 1
                 self.data["species"] = species
             elif "outcell: Unit cell vectors (Ang):" in lines[i]:
@@ -129,8 +153,7 @@ class ReadSiestaOutput:
                         linesplit = lines[j].split() ; magmom.append(linesplit[4:7])
                 else:
                     print("ERROR: Incorret number of spin components !")
-                magmom = np.array(magmom) # New
-                magmom = magmom[sorted_indices] # New
+                magmom = np.array(magmom)[sorted_indices] # New
                 self.data["magmom"] = np.array(magmom).astype("float")
 
     def _validate_keys(self):
